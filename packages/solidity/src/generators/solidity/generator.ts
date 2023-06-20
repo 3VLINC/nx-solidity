87ad8a636f6a8c7dfd324a1d309aa6e0c5f4f83b
Dan Imbrogno <dan@3vl.ca>
2023-06-20 10:17:52 -0400

    Improve typing

--- packages/solidity/src/generators/solidity/generator.ts
+++ packages/solidity/src/generators/solidity/generator.ts
@@ -111,7 +111,7 @@ export async function solidityGenerator(
   });
   
   updateJson(tree, join(normalizedOptions.appProjectRoot, 'tsconfig.app.json'), (pkgJson) => {
-    pkgJson.compilerOptions.types = [...pkgJson.compilerOptions.types, "@nomiclabs/hardhat-ethers"];
+    pkgJson.compilerOptions.types = [...pkgJson.compilerOptions.types, "@nomiclabs/hardhat-ethers", "./typechain-types/hardhat.d.ts", "./typechain-types/index.ts"];
     pkgJson.exclude = ["hardhat.config.ts"],
     pkgJson.include = ["scripts/**/*.ts", "test/**/*.ts"]
     return pkgJson;
