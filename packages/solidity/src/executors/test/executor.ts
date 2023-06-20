69d0246a9e57cb78e75f1afd898023b53a542784
Dan Imbrogno <dan@3vl.ca>
2023-06-20 09:58:08 -0400

    fix output of test command

--- packages/solidity/src/executors/test/executor.ts
+++ packages/solidity/src/executors/test/executor.ts
@@ -6,21 +6,22 @@ import { dirname, resolve } from 'path';
 
 export default async function runExecutor(options: TestExecutorSchema, context: ExecutorContext) {
 
-  
-  const project = context.projectGraph.nodes[context.projectName];
-
   const cwd = dirname(resolve(context.root, options.hardhatConfig));
-  
-  const { stdout, stderr } = await promisify(exec)(
-    `npx hardhat test`,
-    {
-      cwd
-    }
-  );
-  console.log(stdout);
-  console.error(stderr);
 
-  const success = !stderr;
-  return { success };
+  try {
+
+    const { stdout } = await promisify(exec)(
+      `npx hardhat test`,
+      {
+        cwd
+      }
+      );
+      console.log(stdout);
+      return { success: true };
+    } catch (e) {
+      console.error(e.stdout);
+      return { success : false}
+    }
+
   
 }
