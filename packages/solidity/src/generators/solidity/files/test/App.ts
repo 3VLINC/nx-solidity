d16f4c3441ff9de095d868b7a5d6386cde1c83cc
Dan Imbrogno <dan@3vl.ca>
2023-06-20 09:58:23 -0400

    add typing to test

--- packages/solidity/src/generators/solidity/files/test/App.ts
+++ packages/solidity/src/generators/solidity/files/test/App.ts
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
 import { ethers } from "hardhat";
-
+import { App__factory } from '../typechain-types';
 describe('App', async function () {
   it('When calling hello should return "world!"', async () => {
     const [contractOwner] = await ethers.getSigners();
-    const appInstance = await ethers.getContractFactory('App');
+    const appInstance = await ethers.getContractFactory<[], App__factory>('App');
     
     const app = await appInstance.connect(contractOwner).deploy();
     
     const res = await app.hello();
     
-    return expect(res).to.be('world');
+    return expect(res).to.be.equal('world');
   });
 });
