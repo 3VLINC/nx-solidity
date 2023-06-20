dcb1b37cb401fa4954daeb32d9f0c41b49989912
Dan Imbrogno <dan@3vl.ca>
2023-06-20 10:18:38 -0400

    Remove unnecessary type import

--- packages/solidity/src/generators/solidity/files/test/App.ts
+++ packages/solidity/src/generators/solidity/files/test/App.ts
@@ -1,10 +1,9 @@
 import { expect } from 'chai';
 import { ethers } from "hardhat";
-import { App__factory } from '../typechain-types';
 describe('App', async function () {
   it('When calling hello should return "world!"', async () => {
     const [contractOwner] = await ethers.getSigners();
-    const appInstance = await ethers.getContractFactory('App') as App__factory;
+    const appInstance = await ethers.getContractFactory('App');
     
     const app = await appInstance.connect(contractOwner).deploy();
     
